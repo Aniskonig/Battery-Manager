e17c8b6203365483583b8dd4ee5c9af0b8dd4e21
Mohammed Anis Oukebdane <aniskonig13@gmail.com>
2024-01-25 14:39:55 +0300

    Fix duplicate message box problem

--- main.py
+++ main.py
@@ -1,19 +1,24 @@
 import psutil
-import tkinter as tk
 from tkinter import ttk, messagebox
 from playsound import playsound
 import threading
+import tkinter
 
-from PyQt5.QtWidgets import QMessageBox, QApplication
-import sys
-
+root = tkinter.Tk()
 isTimeForReward = False
 
 
-def main():
+def getIsTimeForReward():
+    return isTimeForReward
+
+
+def setIsTimeForReward(val):
     global isTimeForReward
-    print("isTimeForReward", isTimeForReward)
-    # try:
+    isTimeForReward = val
+
+
+def main():
+    print("isTimeForReward", getIsTimeForReward())
     threading.Timer(5.0, main).start()
     battery = psutil.sensors_battery()
     plugged = battery.power_plugged
@@ -21,32 +26,30 @@ def main():
 
     if (int(percent) >= 78) and plugged:
         playsound('unplug.mp3')
-        if not isTimeForReward:
-            messagebox.showinfo("Info", "Please unplug your laptop charger")
+        if not getIsTimeForReward():
+            setIsTimeForReward(True)
 
-        isTimeForReward = True
-        # show_fullscreen_message("Please unplug your laptop charger")
-        # messagebox.showinfo("Info", "Please unplug your laptop charger")
+            root.wm_attributes("-topmost", 1)
+            root.withdraw()
+
+            tkinter.messagebox.showinfo("Info", "Please unplug your laptop charger")
+            root.destroy()
 
     if (int(percent) <= 32) and not plugged:
         playsound('plugin.mp3')
 
-        if not isTimeForReward:
-            messagebox.showinfo("Info", "Please plugin your laptop charger")
+        if not getIsTimeForReward():
+            setIsTimeForReward(True)
 
-        isTimeForReward = True
+            root.wm_attributes("-topmost", 1)
+            root.withdraw()
 
-        # show_fullscreen_message("Please plugin your laptop charger")
+            tkinter.messagebox.showinfo("Info", "Please plugin your laptop charger")
+            root.destroy()
 
-    if ((int(percent) <= 32) and plugged) or ((int(percent) >= 78) and not plugged) and isTimeForReward:
+    if ((int(percent) <= 32) and plugged) or ((int(percent) >= 78) and not plugged) and getIsTimeForReward():
+        setIsTimeForReward(False)
         playsound('welldone.mp3')
-
-        isTimeForReward = False
-        # close_fullscreen()
-    # except Exception as e:
-    #     print("Error", str(e))
-    #     messagebox.showerror("Error", str(e))
-    #     pass
 
 
 if __name__ == "__main__":
